41dd0fc68692c387c6fae71bc03dba0f49811985
wassanaboonna <boonnawassana@gmail.com>
2021-08-20 20:43:27 +0700

    add homepage

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNZCxe9mI_M3ZLOwXj4FZo7D96QkkGFKg",
+  authDomain: "linkedin-clone-wassana.firebaseapp.com",
+  projectId: "linkedin-clone-wassana",
+  storageBucket: "linkedin-clone-wassana.appspot.com",
+  messagingSenderId: "1082216022126",
+  appId: "1:1082216022126:web:66d190f24dcada9831267f",
+  measurementId: "G-66ZL23VYN0"
+};
+
